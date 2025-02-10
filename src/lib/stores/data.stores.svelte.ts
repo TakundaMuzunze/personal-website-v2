6b2f532a2c88bcd61dfd87f084b2381ee0d9278a
Takunda <takunda.muzunze@isitetv.com>
2025-02-10 21:43:24 +0000

    Create stores file to dynamically display navbar links.

---
+++ src/lib/stores/data.stores.svelte.ts
@@ -0,0 +1,3 @@
+export const links = $state({
+  navLinks: [{ label: 'Home' }, { label: 'About Me' }, { label: 'Experience' }, { label: 'Skills' }, { label: 'Projects' }, { label: 'Contact Me' }],
+});
