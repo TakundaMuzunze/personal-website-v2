9507f709d8af660db7a7f73f31c38364fece7059
Takunda <takunda.muzunze@isitetv.com>
2025-02-11 20:54:51 +0000

    Feat(TM): Create hero section component with responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
-	import Header from '$lib/components/Header.svelte';
+  import Header from '$lib/components/Header.svelte';
+  import Hero from '$lib/components/Hero.svelte';
 </script>
 
 <Header />
+<main class="h-screen">
+  <Hero />
+</main>
