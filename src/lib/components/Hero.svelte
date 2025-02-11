1fed51182e2d0bfff687140d3261a7eb0629caf6
Takunda <takunda.muzunze@isitetv.com>
2025-02-11 22:23:10 +0000

    Style: Amend font size

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -4,9 +4,9 @@
 <section class="mx-auto flex h-screen max-w-5xl items-center justify-center p-5">
   <div class="flex flex-col gap-10 md:flex-row lg:gap-20">
     <div class="flex max-w-[468px] flex-col gap-3 lg:gap-5">
-      <h2 class="md:text-lg lg:text-2xl">Hello there👋. My name is,</h2>
-      <h1 class="text-2xl font-bold md:text-3xl lg:text-5xl">Takunda Muzunze</h1>
-      <p class="md:text-lg lg:text-2xl">I design and develop user-centric experiences</p>
+      <h2 class="text-lg lg:text-2xl">Hello there👋. My name is,</h2>
+      <h1 class=" text-3xl font-bold lg:text-5xl">Takunda Muzunze</h1>
+      <p class="text-lg lg:text-2xl">I design and develop user-centric experiences</p>
       <button
         class="w-fit transform cursor-pointer rounded-xl bg-[#006FFF] px-4 py-3 font-semibold text-white transition-transform duration-300 hover:scale-105 lg:text-lg"
         >Get in touch</button
