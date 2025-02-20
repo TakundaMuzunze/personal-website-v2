92c9c68c3e26e4b25d3bb5c2b5f4ff2f848db2fe
Takunda <takunda.muzunze@isitetv.com>
2025-02-20 21:30:26 +0000

    Feat(TM): Create card component to fetch from JSON file and display data in cards.

--- src/lib/components/Experience/Experience.svelte
+++ src/lib/components/Experience/Experience.svelte
@@ -6,7 +6,7 @@
 <section class="mx-auto flex h-screen max-w-5xl snap-start items-center p-5">
   <div class="flex w-full flex-col items-start justify-start gap-10 lg:gap-20">
     <div class="flex max-w-2xl flex-col gap-3 lg:gap-5">
-      <h3 class="font-semibold text-[#006FFF] uppercase md:text-lg lg:text-xl">Experience</h3>
+      <h3 class="text-dark-blue font-semibold uppercase md:text-lg lg:text-xl">Experience</h3>
       <h2 class="text-2xl font-bold capitalize md:text-3xl lg:text-4xl">My Professional Journey</h2>
       <p class="lg:text-lg">
         Explore my journey so far, from university to internships and beyond. Here's a look at the key experiences that have shaped my development as
---
+++ src/lib/components/Projects/Card.stories.svelte
@@ -0,0 +1,10 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Card from './Card.svelte';
+
+  const { Story } = defineMeta({
+    component: Card,
+  });
+</script>
+
+<Story name="Project Card" />
