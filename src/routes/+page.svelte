e27c9f93a66cc207acd2c04fcc3145076811959f
Takunda <takunda.muzunze@isitetv.com>
2025-02-20 17:45:29 +0000

    Feat(TM): Amend folder name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import AboutMe from '$lib/components/About/AboutMe.svelte';
   import Header from '$lib/components/Header.svelte';
   import Hero from '$lib/components/Hero.svelte';
-  import ProjectSection from '$lib/components/MyWork/ProjectSection.svelte';
+  import ProjectSection from '$lib/components/Experience/ProjectSection.svelte';
 </script>
 
 <main class="h-screen snap-y snap-mandatory overflow-y-auto scroll-smooth">
